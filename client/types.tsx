909097dcbcd97848d07a3f3202a2bcfd98625393
Alan Cheung <kahoalan@gmail.com>
2020-08-24 00:26:37 +0800

    frontend init

---
+++ client/types.tsx
@@ -0,0 +1,17 @@
+export type RootStackParamList = {
+  Root: undefined;
+  NotFound: undefined;
+};
+
+export type BottomTabParamList = {
+  Dashboard: undefined;
+  Logout: undefined;
+};
+
+export type TabOneParamList = {
+  TabOneScreen: undefined;
+};
+
+export type TabTwoParamList = {
+  TabTwoScreen: undefined;
+};
